47aa65356bbdbf38908e90e1f3dc7c714ce5ab6d
maxium0526 <maxium0526@gmail.com>
2020-05-10 20:13:35 +0800

    add SingleLineHand class

--- Hand.js
+++ Hand.js
@@ -5,7 +5,7 @@ class Hand{
 		this.y = config.y;
 		this.length = config.length;
 		this.degree = (config.degree+360) % 360;//
-		this.color = config.color;
+		this.color = config.color || '#000000';
 	}
 	rotate(degree){
 		this.degree += degree%360;
@@ -18,6 +18,33 @@ class Hand{
 		ctx.beginPath();
 		ctx.moveTo(this.x, this.y);
 		ctx.lineTo(xp, yp);
+		ctx.stroke();
+	}
+}
+
+class SingleLineHand extends Hand{
+	constructor(config){
+		super(config);
+		this.length = {};
+		if(Array.isArray(config.length)){
+			this.length.head = config.length[0];
+			this.length.body = config.length[1];
+		} else {
+			this.length.head = 0;
+			this.length.body = config.length;
+		}
+		this.width = config.width;
+	}
+	draw(ctx){
+		let xbp = this.x + Math.cos(this.degree * Math.PI / 180) * this.length.body;
+		let ybp = this.y - Math.sin(this.degree * Math.PI / 180) * this.length.body;
+		let xhp = this.x - Math.cos(this.degree * Math.PI / 180) * this.length.head;
+		let yhp = this.y + Math.sin(this.degree * Math.PI / 180) * this.length.head;
+		ctx.color = this.color;
+		ctx.lineWidth = this.width;
+		ctx.beginPath();
+		ctx.moveTo(xhp, yhp);
+		ctx.lineTo(xbp, ybp);
 		ctx.stroke();
 	}
 }
